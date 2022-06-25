f318a2470d0897ee6d708f218b5fd0e5c6f3c773
youngkwon02 <youngkwon02@naver.com>
2022-06-26 01:40:09 +0900

    refactor: OrderApp to use AppConfig

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,15 +3,15 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
-import hello.core.order.OrderServiceImpl;
+import hello.core.order.OrderService;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberServiceImpl memberService = new MemberServiceImpl();
-        OrderServiceImpl orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
