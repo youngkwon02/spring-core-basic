d547641d89f737e86eeb00f8c43229668e9bcca4
youngkwon02 <youngkwon02@naver.com>
2022-06-30 16:23:50 +0900

    feat: ConfigurationSingletonTest

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
