7bbdd0c119f45cbf3425fdc5c0468f149791f437
youngkwon02 <youngkwon02@naver.com>
2022-07-03 21:07:34 +0900

    refactor: NetworkClient implements InitializingBean and DisposableBean

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -11,7 +11,7 @@ public class BeanLifeCycleTest {
     @Test
     public void lifeCycleTest() {
         ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
-        NetworkClient client = ac.getBean(NetworkClient.class); 
+        NetworkClient client = ac.getBean(NetworkClient.class);
         ac.close();
     }
 
